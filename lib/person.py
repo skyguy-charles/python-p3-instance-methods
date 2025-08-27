7a87f31644d8dde694138c075e9008e50b8188a2
charles <cyphersmae@gmail.com>
2025-08-27 13:49:48 +0300

    instance

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,24 @@
-#!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
+    def talk(self):
+        print("Hello World!")
 
-    #Instance method definition
-    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lib/walk.py
@@ -0,0 +1,26 @@
+# walk.py
+from person import Person
+
+class Person(Person):
+    def walk(self):
+        print("The person is walking.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
